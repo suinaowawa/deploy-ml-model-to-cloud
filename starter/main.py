64b8523260f3943815f23bbd94972319def5d369
Luna <yc@wallace.com.au>
2024-01-04 19:30:33 +1100

    LC: added example values in schema

--- starter/main.py
+++ starter/main.py
@@ -49,6 +49,28 @@ class InputData(BaseModel):
     hours_per_week: int
     native_country: str
 
+    class Config:
+        schema_extra = {
+            "examples": [
+                {
+                    "age": 39,
+                    "workclass": "State-gov",
+                    "fnlgt": 77516,
+                    "education": "Bachelors",
+                    "education_num": 13,
+                    "marital_status": "Never-married",
+                    "occupation": "Adm-clerical",
+                    "relationship": "Not-in-family",
+                    "race": "White",
+                    "sex": "Male",
+                    "capital_gain": 2174,
+                    "capital_loss": 0,
+                    "hours_per_week": 40,
+                    "native_country": "United-States",
+                }
+            ]
+        }
+
 
 # Mock model inference function
 def predict_salary(data: InputData) -> str:
